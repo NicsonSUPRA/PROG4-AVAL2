e9464ca6d8cc6e7edb63c8db91e8b9a2a6c5c8f5
NicsonSUPRA <nicsoncc1234@gmail.com>
2024-06-11 21:01:33 -0300

    add description

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,12 +1,12 @@
 class Transaction {
   final String id;
   final String title;
-  final double value;
+  final String description;
   final DateTime date;
 
   Transaction(
       {required this.id,
       required this.title,
-      required this.value,
+      required this.description,
       required this.date});
 }
